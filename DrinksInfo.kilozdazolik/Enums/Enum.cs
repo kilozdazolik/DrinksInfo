5a64dba2418c2bce2cb1baed6b442198ab783311
kilozdazolik <paulsonzoltan@protonmail.com>
2025-10-30 19:28:55 +0100

    fix: added enum to git

---
+++ DrinksInfo.kilozdazolik/Enums/Enum.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace DrinksInfo.kilozdazolik.Enums;
+
+internal enum MenuAction {
+    [Description("View Categories")]
+    ViewCategories,
+
+    [Description("View Drinks")]
+    ViewDrinks,
+
+    [Description("View Drink Details")]
+    ViewDrinkDetails,
+    
+    [Description("View Favorite Drinks")]
+    ViewFavorite,
+
+    [Description("Exit")]
+    Exit
+}
